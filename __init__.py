7d358f2f975df8cdb3d1d802e5f146547ecc26fd
Yonezcy <364251097@qq.com>
2018-06-05 19:31:58 +0800

    修改__init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""Makes helper libraries available in the cifar10 package."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cifar10
+import cifar10_input
